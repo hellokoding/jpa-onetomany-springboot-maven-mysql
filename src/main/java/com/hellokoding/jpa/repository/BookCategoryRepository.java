aad222c0c66f44cca274855ba652edce94365aa1
Giau Ngo <giaunv@gmail.com>
2016-03-25 08:52:10 +0700

    Update spring.jpa.hibernate.ddl-auto=validate

--- src/main/java/com/hellokoding/jpa/repository/BookCategoryRepository.java
+++ src/main/java/com/hellokoding/jpa/repository/BookCategoryRepository.java
@@ -2,6 +2,8 @@ package com.hellokoding.jpa.repository;
 
 import com.hellokoding.jpa.model.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Integer>{
 }
